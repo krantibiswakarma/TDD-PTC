3028c41a534581753b668fda9e644fe55eae312d
hiteshdhingra <hdhingra@ptc.com>
2017-02-16 17:45:21 +0530

    initial comint

---
+++ src/main/java/com/ptc/university/mastermind/MyFirstClass.java
@@ -0,0 +1,13 @@
+package com.ptc.university.mastermind;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyFirstClass
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ptc/university/mastermind/MyFirstClassTest.java
@@ -0,0 +1,14 @@
+package com.ptc.university.mastermind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyFirstClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
